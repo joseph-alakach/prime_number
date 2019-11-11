4681a2ce802e71741092aa5e3d8c915fb3240295
joseph-alakach <57460061+joseph-alakach@users.noreply.github.com>
2019-11-12 00:01:58 +0400

    this code is to show the prime numbers between two positive numbers

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const util = require("./myUtils.js");
-
-console.log(util.isPrime(2));
-
+const any=require("readline-sync");
+let x=any.questionInt("enter the first positive integer: ");
+// x is the first number
+const y=any.questionInt("enter the second positive integer: ");
+// y is the second number 
+let prime_numbers=[]
+for(;x<=y;x++){
+    if(util.isPrime(x)){
+        prime_numbers.push(x);
+    }
+}
+console.log(prime_numbers);
